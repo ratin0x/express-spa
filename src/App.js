35c8f14706e6a0bd636c71db383b1c07d44c16bd
Paul Fox <paulf.ratinox@gmail.com>
2019-01-29 11:27:15 +0000

    Adds form fields and some basic styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 class App extends Component {  
@@ -16,11 +15,30 @@ class App extends Component {
         </header>
         <form className="user-form" onSubmit={this.submitHandler}>
           <div className="form-field">
-            <label htmlFor="title">Title</label>
+            <label htmlFor="title">Title*</label>
             <input type="text" id="title" name="title" placeholder="e.g. Dr, Mrs, Mr, Ms" required></input>
           </div>
+
+          <div className="form-field">
+            <label htmlFor="name">Name*</label>
+            <input type="text" id="name" name="name" placeholder="" required></input>
+          </div>
+
+          <div className="form-field">
+            <label htmlFor="dob">Date of Birth*</label>
+            <input type="text" id="dob" name="dob" placeholder="" required></input>
+          </div>
+
+          <div className="form-controls">
+            <button type="submit">Submit</button>        
+          </div>
+
+          <div className="form-hints">
+            <span>
+              <small>* denotes a required field</small>
+            </span>
+          </div>
         </form>
-        <button type="submit">Submit</button>        
       </div>
     );
   }
